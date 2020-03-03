9fed5a7c3b790a4859de958f94d5a3592e1701b7
LEE HYEONSEOK <simon@LEEui-MacBookPro.local>
2020-03-03 17:48:01 +0900

    trial for step2

--- src/components/VideoListEntry.js
+++ src/components/VideoListEntry.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { fakeData } from './__test__/fakeData';
-// console.log(fakeData)
 
-const newTitle = [];
+const newUrl = []
+for(let val of fakeData){
+  newUrl.push(val.snippet.thumbnails.default.url);
+}
+
+const newTitle = []
 for(let val of fakeData){
   newTitle.push(val.snippet.title);
 }
-console.log(newTitle);
+// console.log(newTitle);
+const newDescriptrion = []
+for(let val of fakeData){
+  newDescriptrion.push(val.snippet.description);
+}
+
 
 const VideoListEntry = () => (
   <div className="video-list-entry">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+      {/* <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" /> */}
+      <img className="media-object" src={`${newUrl.shift()}`} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title"> {`${newTitle}`} </div>
-      <div className="video-list-entry-detail"> {`${fakeData.forEach(el => {el.snippet.description})}`} </div>
+      <div className="video-list-entry-title"> {`${newTitle.shift()}`} </div>
+      <div className="video-list-entry-detail"> {`${newDescriptrion.shift()}`} </div>
     </div>
   </div>
 );
