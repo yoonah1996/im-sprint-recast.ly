b1894a74dde34b73b5554315a643a19ca6f30758
LEE HYEONSEOK <simon@LEEui-MacBookPro.local>
2020-03-03 22:57:47 +0900

    completed step2 vidoeListEntry

--- src/components/VideoListEntry.js
+++ src/components/VideoListEntry.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { fakeData } from './__test__/fakeData';
 
 const VideoListEntry = (fakeData) => (
   <div className="video-list-entry">
     <div className="media-left media-middle">
       {/* <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" /> */}
-      <img className="media-object" src={fakeData.snippet.thumbnails.default.url} alt="" />
+      <img className="media-object" src={fakeData.value.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title"> {fakeData.snippet.title} </div>
-      <div className="video-list-entry-detail"> {fakeData.snippet.description} </div>
+      <div className="video-list-entry-title"> {fakeData.value.snippet.title} </div>
+      <div className="video-list-entry-detail"> {fakeData.value.snippet.description} </div>
     </div>
   </div>
 );
-
-console.log(fakeData)
 
 
 export default VideoListEntry;
