e5fd02c1a57692b4dba6113f60fe2042f71a5908
yoonah1996 <yoonah1996@gmail.com>
2020-03-03 23:30:37 +0900

    Fix

--- src/components/VideoListEntry.js
+++ src/components/VideoListEntry.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { fakeData } from './__test__/fakeData';
 
-const posts = []
-const post = {}
-
-for(let val of fakeData) {
-  post["url"] = val.snippet.thumbnails.default.url;
-  post["title"] = val.snippet.title 
-  post["descript"] =val.snippet.description
-  posts.push(JSON.parse(JSON.stringify(post)))
-}
-
- const VideoListEntry  = () => (
+const VideoListEntry = (fakeData) => (
   <div className="video-list-entry">
     <div className="media-left media-middle">
       {/* <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" /> */}
-      <img className="media-object" key = {idx} src={post.url} alt="" />
+      <img className="media-object" src={fakeData.value.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title"> {``} </div>
-      <div className="video-list-entry-detail"> {``} </div>
+      <div className="video-list-entry-title"> {fakeData.value.snippet.title} </div>
+      <div className="video-list-entry-detail"> {fakeData.value.snippet.description} </div>
     </div>
   </div>
 ));
